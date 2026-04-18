703f0841cc05a9ed27c06c2fe753fca9876d466c
bulldogofthepines <bulldogofthepines@gmail.com>
2026-04-17 21:00:20 -0500

    Update ghobot_mirror.py

--- ghobot_mirror.py
+++ ghobot_mirror.py
@@ -126,7 +126,7 @@ def generate_ghost_mirror():
         display_condition = "New" if "new" in cond_raw else "Used"
         
         # FIXED: Added literal /itm/ and slashes
-        ebay_url = "https://ebay.com/itm/" + item_id
+        ebay_url = "https://www.ebay.com/itm/" + item_id
         
         product_div = f"""
         <div class="product">
@@ -172,7 +172,7 @@ def generate_ghost_mirror():
             const itemId = urlParams.get('id');
             if (itemId && itemId.length > 5) {
                 // FIXED: Literal redirect string
-                window.location.replace("https://ebay.com/itm/" + itemId);
+                window.location.replace("https://www.ebay.com/itm/" + itemId);
             }
         };
     </script>
